d6971af3e8c520c93c53e2f38e22aadcd815ca6e
emmachen12 <emc1207pi@gmail.com>
2024-02-26 14:58:35 -0500

    hi

---
+++ Circle.java
@@ -0,0 +1,14 @@
+public class Circle 
+{
+    public static void main (String[]args)
+    {
+        double radius = 4.28;
+        double area = 0;
+        double circumference = 0;
+        area = Math.PI*radius*radius;
+        circumference = 2*Math.PI*radius;
+        System.out.println("The area of the cricle is " + area);
+        System.out.println("The circumference of the cricle is " + circumference);
+    }
+
+}
---
+++ Rect.java
@@ -0,0 +1,12 @@
+public class Rect{
+    public static void main(String[] args)
+    {
+    System.out.println(" ***************");
+    System.out.println("*               *");
+    System.out.println("*               *");
+    System.out.println("*               *");
+    System.out.println("*               *");
+    System.out.println("*               *");
+    System.out.println(" ***************");
+    }
+}
