1f0ea327f9f3aa47ba7233bd8c4733a84f0215a4
emmachen12 <emc1207pi@gmail.com>
2024-02-28 14:29:54 -0500

    h

--- Digits.java
+++ Digits.java
@@ -1,13 +1,20 @@
-public class Digits {
+public class Digits
+{
     public static void main (String[]args)
     {
     int num;
-    int hund;
-    int tens;
     int ones;
+    int tens;
+    int hunds;
 
-    num = 526;
-    hund = 888;
+    num = 258;
+    ones = num % 10;
+    tens = ((num / 10) % 10);
+    hunds = (num-(tens*10)-(ones))/100;
+
+    System.out.println("The hundreds place digit is " + hunds);
+    System.out.println("The tens place digit is " + tens);
+    System.out.println("The ones place digit is " + ones);
 
     }
 }
