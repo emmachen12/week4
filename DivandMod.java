5eb231b251f3b136bb859d6efe719d1db68316a5
emmachen12 <emc1207pi@gmail.com>
2024-03-01 14:34:38 -0500

    h

---
+++ DivandMod.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DivandMod{
+
+    public static void main (String[] args)
+    {
+    int integer;
+    int integer1;
+
+    int modular;
+    int modular1;
+    int quotient;
+    int quotient2;
+
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter an integer:");
+    integer = input.nextInt();
+    System.out.print("Enter a second integer:");
+    integer1 = input.nextInt();
+    input.close();
+
+    modular = int
+    }
+}
