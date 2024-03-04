aabfc9794acbd5074746b6b679bba46f0e804b96
emmachen12 <emc1207pi@gmail.com>
2024-03-04 14:39:37 -0500

    h

--- AccountSetup.java
+++ AccountSetup.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
+
 public class AccountSetup {
-    
+    public static void main (String[]args)  
+    {
+    int name;
+    int pass;
+
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your full name:");
+    name = input.nextInt();
+    System.out.print("Enter your password:");
+    pass = input.nextInt();
+    input.close();
+
+
+    System.out.println("Your name is " + name);
+    System.out.println("Your password " + pass);
+    }
 }
--- TempConverter.java
+++ TempConverter.java
@@ -1,3 +1,21 @@
+import java.util.* ;
 public class TempConverter {
-    
+    public static void main (String[]args)
+    {
+    int fah;
+    int cel;
+
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a Fahrenheit degree:");
+    fah = input.nextInt();
+    input.close();
+
+
+    cel = (5 / 9 * (fah - 32));
+
+
+    System.out.println("Your temperature in Celsius is " + cel);
+    }
+
 }
